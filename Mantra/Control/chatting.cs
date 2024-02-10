415d02fc9416d7b0248e8f0f3f3bd0d95d8716e4
krqus <90345430+krqus@users.noreply.github.com>
2024-02-10 15:25:46 -0600

    Update chatting.cs

--- Mantra/Control/chatting.cs
+++ Mantra/Control/chatting.cs
@@ -18,6 +18,7 @@ namespace Mantra.Control
             InitializeComponent();
         }
 
+        // Attempts to send a message through the specified webhook.
         private void guna2Button2_Click(object sender, EventArgs e)
         {
             try
@@ -39,16 +40,19 @@ namespace Mantra.Control
             }
         }
 
+        // When the textbox's content is changed it will attempt to load the image provided.
         private void guna2TextBox1_TextChanged(object sender, EventArgs e)
         {
             guna2CirclePictureBox1.LoadAsync(guna2TextBox1.Text);
         }
 
+        // Real-time username updating on preview
         private void guna2TextBox3_TextChanged(object sender, EventArgs e)
         {
             label3.Text = guna2TextBox3.Text;
         }
-
+        
+        // Real-time text updating on preview
         private void guna2TextBox4_TextChanged(object sender, EventArgs e)
         {
             label4.Text = guna2TextBox4.Text;
